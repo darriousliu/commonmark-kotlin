e83ab3c0d3cd4990d9aaf041c1f0c276233eef43
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2025-07-30 14:06:47 +0800

    feat(build): 集成 Maven Central 发布插件并优化发布配置
    - 引入 `com.vanniktech.maven.publish` 插件，支持简化的 Maven Central 发布流程
    - 更新 `libs.versions.toml` 文件，新增 Maven Publish 插件的版本声明
    - 优化 `publishing` 和 `MavenPublication` 配置，确保多平台发布兼容性
    - 改善 `pom` 信息配置，集中声明项目信息，提高可维护性
    - 保留 GitHub Packages 发布配置，支持多元化的发布渠道

--- build-convention/src/main/kotlin/publish.gradle.kts
+++ build-convention/src/main/kotlin/publish.gradle.kts
@@ -1,20 +1,40 @@
 plugins {
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
 }
 
 group = "io.github.darriousliu"
 version = "0.25.0"
+
+val commonPom = Action<MavenPom> {
+    name.set("Commonmark-Kotlin")
+    description.set("A multiplatform library for parsing CommonMark Markdown syntax in Kotlin")
+    url.set("https://github.com/darriousliu/commonmark-kotlin")
+
+    licenses {
+        license {
+            name.set("BSD 2-Clause License")
+            url.set("https://opensource.org/licenses/BSD-2-Clause")
+        }
+    }
+    developers {
+        developer {
+            id.set("darriousliu")
+            name.set("Darrious Liu")
+        }
+    }
+    scm {
+        url.set("https://github.com/darriousliu/commonmark-kotlin")
+        connection.set("scm:git:git://github.com/darriousliu/commonmark-kotlin.git")
+        developerConnection.set("scm:git:git://github.com/darriousliu/commonmark-kotlin.git")
+    }
+}
 
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/darriousliu/commonmark-kotlin")
-            credentials {
-                username = System.getenv("GITHUB_PUBLISH_USERNAME")
-                password = System.getenv("GITHUB_PUBLISH_TOKEN")
-            }
+            credentials(PasswordCredentials::class)
         }
     }
     publications {
@@ -22,23 +42,15 @@ publishing {
             groupId = project.group.toString()
             version = project.version.toString()
 
-            pom {
-                name.set("Commonmark-Kotlin")
-                description.set("A multiplatform library for parsing CommonMark Markdown syntax in Kotlin")
-                url.set("https://github.com/darriousliu/commonmark-kotlin")
-
-                licenses {
-                    license {
-                        name.set("BSD 2-Clause License")
-                        url.set("https://opensource.org/licenses/BSD-2-Clause")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/darriousliu/commonmark-kotlin")
-                    connection.set("scm:git:git://github.com/darriousliu/commonmark-kotlin.git")
-                    developerConnection.set("scm:git:git://github.com/darriousliu/commonmark-kotlin.git")
-                }
-            }
+            pom(commonPom)
         }
     }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    pom(commonPom)
 }
